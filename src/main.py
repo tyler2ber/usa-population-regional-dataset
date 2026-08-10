aba81702b4ee4520b8674222342c2cab6d2fdc0a
Tyler <tyler2ber@gmail.com>
2026-08-09 20:53:17 -0400

    Finished "USA POPULATION: STATES"

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 import pandas
 
-# dataset
-df = pandas.read_csv("dataset/usa-population-states-2020.csv")
-print(df.head())
+# todo: create regional dataset
+
+# USA POPULATION: REGIONAL
+print("\n====> USA-POPULATION-REGIONAL dataset\n")
+# todo: top 5 most / top 5 least
+
+# USA POPULATION: STATES
+print("====> USA-POPULATION-STATES dataset\n")
+df_states = pandas.read_csv("dataset/usa-population-states-2020.csv")
+
+# top 5 most populated
+print("top 5 most populated states")
+print(df_states.sort_values(by="Population", ascending=False).head())
+
+# top 5 least populated
+print("\ntop 5 least populated states")
+print(df_states.sort_values(by="Population").head())
+
+print("") # lul
