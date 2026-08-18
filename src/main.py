68848f3b0e3f04eb061cf0e39e9a8c39c4b72189
Tyler <tyler2ber@gmail.com>
2026-08-18 17:59:02 -0400

    RENAME: restructured for usa-population-regions.csv

--- src/main.py
+++ src/main.py
@@ -1,23 +1,10 @@
-import pandas as pd
+import pandas
 
-df_states = pd.read_csv("dataset/usa-population-states.csv")
+regions = {
 
-# CREATE df_regions_coastal
-df_regions_coastal = pd.DataFrame(columns=
-    [
-        "Region(coastal)",
-        "Population"
-    ]
-).astype(
-    {
-        "Region(coastal)": "string",
-        "Population": "int64"
-    }
-)
+    # coastal
 
-# rows for df_regions_coastal
-row_eastcoast = df_states.loc[df_states["State"].isin(
-    [
+    "East Coast": [
         "Maine",
         "New Hampshire",
         "Vermont",
@@ -35,61 +22,30 @@ row_eastcoast = df_states.loc[df_states["State"].isin(
         "South Carolina",
         "Georgia",
         "Florida"
-    ]
-), "Population"].sum()
-df_regions_coastal.loc[len(df_regions_coastal)] = ["East Coast", row_eastcoast]
-
-row_greatlakes = df_states.loc[df_states["State"].isin(
-    [
+    ],
+    "Great Lakes": [
         "Wisconsin",
         "Illinois",
         "Indiana",
         "Michigan",
         "Ohio"
-    ]
-), "Population"].sum()
-df_regions_coastal.loc[len(df_regions_coastal)] = ["Great Lakes", row_greatlakes]
-
-row_gulfcoast = df_states.loc[df_states["State"].isin(
-    [
+    ],
+    "Gulf Coast": [
         "Texas",
         "Louisiana",
         "Mississippi",
         "Alabama",
         "Florida"
-    ]
-), "Population"].sum()
-df_regions_coastal.loc[len(df_regions_coastal)] = ["Gulf Coast", row_gulfcoast]
-
-row_westcoast = df_states.loc[df_states["State"].isin(
-    [
+    ],
+    "West Coast": [
         "Washington",
         "Oregon",
         "California"
-    ]
-), "Population"].sum()
-df_regions_coastal.loc[len(df_regions_coastal)] = ["West Coast", row_westcoast]
+    ],
 
-# print and create
-print(f"\n{df_regions_coastal.head()}\n")
-df_regions_coastal.to_csv("dataset/usa-population-regions-coastal.csv", index=False)
+    # coastal (custom)
 
-# CREATE df_regions_coastal_custom
-df_regions_coastal_custom = pd.DataFrame(columns=
-    [
-        "Region(coastal)[custom]",
-        "Population"
-    ]
-).astype(
-    {
-        "Region(coastal)[custom]": "string",
-        "Population": "int64"
-    }
-)
-
-# rows for df_regions_coastal_custom
-row_eastcoast_north = df_states.loc[df_states["State"].isin(
-    [
+    "East Coast (north)": [
         "Maine",
         "New Hampshire",
         "Vermont",
@@ -103,50 +59,114 @@ row_eastcoast_north = df_states.loc[df_states["State"].isin(
         "Delaware",
         "West Virginia",
         "Virginia"
-    ]
-), "Population"].sum()
-df_regions_coastal_custom.loc[len(df_regions_coastal_custom)] = ["East Coast north", row_eastcoast_north]
+    ],
 
-row_eastcoast_south = df_states.loc[df_states["State"].isin(
-    [
+    "East Coast (south)": [
         "North Carolina",
         "South Carolina",
         "Georgia",
         "Florida"
+    ],
+
+    # w/ other
+
+    "other": [
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "Colorado",
+        "Hawaii",
+        "Idaho",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Minnesota",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Mexico",
+        "North Dakota",
+        "Oklahoma",
+        "South Dakota",
+        "Tennessee",
+        "Utah",
+        "Wyoming"
     ]
-), "Population"].sum()
-df_regions_coastal_custom.loc[len(df_regions_coastal_custom)] = ["East Coast south", row_eastcoast_south]
 
-df_regions_coastal_custom.loc[len(df_regions_coastal_custom)] = ["Great Lakes", row_greatlakes]
-df_regions_coastal_custom.loc[len(df_regions_coastal_custom)] = ["Gulf Coast", row_gulfcoast]
-df_regions_coastal_custom.loc[len(df_regions_coastal_custom)] = ["West Coast", row_westcoast]
+}
 
-# print and create
-print(f"{df_regions_coastal_custom.head()}\n")
-df_regions_coastal_custom.to_csv("dataset/usa-population-regions-coastal-custom.csv", index=False)
+df_states = pandas.read_csv("dataset/usa-population-states.csv")
 
-# USA-POPULATION-REGIONAL dataset
-print("====> USA-POPULATION-REGIONAL dataset\n")
+# coastal
+population_eastcoast = df_states.loc[df_states["State"].isin(regions["East Coast"]), "Population"].sum()
+population_greatlakes = df_states.loc[df_states["State"].isin(regions["Great Lakes"]), "Population"].sum()
+population_gulfcoast = df_states.loc[df_states["State"].isin(regions["Gulf Coast"]), "Population"].sum()
+population_westcoast = df_states.loc[df_states["State"].isin(regions["West Coast"]), "Population"].sum()
 
-print("top 5 most populated regions(coastal)")
-print(f"{df_regions_coastal.sort_values(by='Population', ascending=False).head()}\n")
+# coastal (custom)
+population_eastcoast_north = df_states.loc[df_states["State"].isin(regions["East Coast (north)"]), "Population"].sum()
+population_eastcoast_south = df_states.loc[df_states["State"].isin(regions["East Coast (south)"]), "Population"].sum()
 
-print("top 5 least populated regions(coastal)")
-print(f"{df_regions_coastal.sort_values(by='Population').head()}\n")
+# w/ other
+population_other = df_states.loc[df_states["State"].isin(regions["other"]), "Population"].sum()
 
-print("top 5 most populated regions(coastal)[custom]")
-print(f"{df_regions_coastal_custom.sort_values(by='Population', ascending=False).head()}\n")
+df_population_regions = pandas.DataFrame(
+    columns=[
+        "Region",
+        "Population"
+    ]
+).astype(
+    {
+        "Region": "string",
+        "Population": "int64"
+    }
+)
+df_population_regions.loc[len(df_population_regions)] = ["East Coast", population_eastcoast]
+df_population_regions.loc[len(df_population_regions)] = ["Great Lakes", population_greatlakes]
+df_population_regions.loc[len(df_population_regions)] = ["Gulf Coast", population_gulfcoast]
+df_population_regions.loc[len(df_population_regions)] = ["West Coast", population_westcoast]
+# ===> save
+df_population_regions.to_csv("dataset/usa-population-regions.csv", index=False)
+print("✓ created usa-population-regions.csv")
 
-print("top 5 least populated regions(coastal)[custom]")
-print(f"{df_regions_coastal_custom.sort_values(by='Population').head()}\n")
+df_population_regions_custom = pandas.DataFrame(
+    columns=[
+        "Region[custom]",
+        "Population"
+    ]
+).astype(
+    {
+        "Region[custom]": "string",
+        "Population": "int64"
+    }
+)
+df_population_regions_custom.loc[len(df_population_regions_custom)] = ["East Coast (north)", population_eastcoast_north]
+df_population_regions_custom.loc[len(df_population_regions_custom)] = ["East Coast (south)", population_eastcoast_south]
+df_population_regions_custom.loc[len(df_population_regions_custom)] = ["Great Lakes", population_greatlakes]
+df_population_regions_custom.loc[len(df_population_regions_custom)] = ["Gulf Coast", population_gulfcoast]
+df_population_regions_custom.loc[len(df_population_regions_custom)] = ["West Coast", population_westcoast]
+# ===> save
+df_population_regions_custom.to_csv("dataset/usa-population-regions_custom.csv", index=False)
+print("✓ created usa-population-regions_custom.csv")
 
-# USA-POPULATION-STATES dataset
-print("====> USA-POPULATION-STATES dataset\n")
-
-# top 5 most populated
-print("top 5 most populated states")
-print(f"{df_states.sort_values(by='Population', ascending=False).head()}\n")
-
-# top 5 least populated
-print("top 5 least populated states")
-print(f"{df_states.sort_values(by='Population').head()}\n")
+df_population_regions_custom_other = pandas.DataFrame(
+    columns=[
+        "Region[custom w/ other]",
+        "Population"
+    ]
+).astype(
+    {
+        "Region[custom w/ other]": "string",
+        "Population": "int64"
+    }
+)
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["East Coast (north)", population_eastcoast_north]
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["East Coast (south)", population_eastcoast_south]
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["Great Lakes", population_greatlakes]
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["Gulf Coast", population_gulfcoast]
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["West Coast", population_westcoast]
+df_population_regions_custom_other.loc[len(df_population_regions_custom_other)] = ["other", population_other]
+# ===> save
+df_population_regions_custom_other.to_csv("dataset/usa-population-regions_custom_other.csv", index=False)
+print("✓ created usa-population-regions_custom_other.csv")
